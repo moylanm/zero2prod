d74d166b5c339e172b6476f2b62144f3a85aca1d
Myles Moylan <moylanm@oregonstate.edu>
2023-08-29 17:44:25 -0700

    run cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,7 @@ use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:0")
-        .expect("Failed to bind to random port");
-    
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
+
     run(listener)?.await
 }
