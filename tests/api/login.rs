4ffda882c7171c364eac63defbbaa63731c03b15
Myles Moylan <myles.moylan@gmail.com>
2023-09-08 14:33:42 -0700

    fix location parameter

--- tests/api/login.rs
+++ tests/api/login.rs
@@ -35,7 +35,7 @@ async fn redirect_to_admin_dashboard_after_login_success() {
         "password": &app.test_user.password
     });
     let response = app.post_login(&login_body).await;
-    assert_is_redirected_to(&response, "location");
+    assert_is_redirected_to(&response, "/admin/dashboard");
 
     // Act - Part 2 - Follow the redirect
     let html_page = app.get_admin_dashboard().await;
